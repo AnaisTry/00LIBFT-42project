009d4347522a14cfd013f74695f37348c979efc7
AnaisTry <63559150+AnaisTry@users.noreply.github.com>
2022-05-03 16:39:59 +0200

    add FT_SPLIT_TESTER

--- 00LIBFT/libft_tester.sh
+++ 00LIBFT/libft_tester.sh
@@ -14,6 +14,7 @@
 
 git clone https://github.com/Tripouille/libftTester.git
 git clone https://github.com/y3ll0w42/libft-war-machine.git
+git clone https://github.com/Ysoroko/FT_SPLIT_TESTER.git
 
 cd ..
 git clone https://github.com/alelievr/libft-unit-test.git
@@ -28,8 +29,11 @@ cd ../libft/libft-war-machine
 cd ../libftTester
 make a
 
+cd ../FT_SPLIT_TESTER
+make
+
 cd ..
-rm -rf libftTester libft-war-machine
+rm -rf libftTester libft-war-machine FT_SPLIT_TESTER
 
 cd ..
 rm -rf libft-unit-test
